11daa0620200624945ce57277848b43536311b6d
Yuan Fang <yxf313@miami.edu>
2020-05-05 15:15:46 -0400

    add age btn

--- project/main.js
+++ project/main.js
@@ -9,15 +9,14 @@ let nest = d3.nest()
 })
     .entries(dataset);
 
-
 let section = portraitsContainer
-    .append("g")
+    .append("div")
     .attr("id", "section");
 
-let architect = section.selectAll('g')
+let architect = section.selectAll('div')
     .data(nest)
     .enter()
-    .append('g')
+    .append('div')
     .attr("id", "architect");
 
 let pictures = architect.selectAll('img')
@@ -31,6 +30,177 @@ let pictures = architect.selectAll('img')
     .on("mouseenter", ShowTooltip)
     .on("mouseout", DismissTooltip);
 
+    const index = d => d.index;
+    function ShowTooltip(d){
+        d3.select("#tooltip" + index(d)).style("opacity", 0.9);
+    }
+    function DismissTooltip(d){
+        d3.select("#tooltip" + index(d)).style("opacity", 0);
+    }
+var architectRemoved = false;
+var architectHadRemovedOnce = false;
+var firstBtnPressedAfterArchitectRemoved = true;
+var AgeImg;
+var Architect;
+var Pictures;
+var PicturesContainer;
+// toggle genderBtn
+    $("#genderBtn")
+    .on("click", onClickGenderBtn);
+    
+    function onClickGenderBtn(){
+        if(!architectRemoved){
+            if(!firstBtnPressedAfterArchitectRemoved){
+                PicturesContainer
+                .attr("class", "otherProperty gender")
+                .attr("src", (d) => "img/gender/" + d.gender + ".png");
+            } else {
+                pictures
+                .attr("class", "otherProperty gender")
+                .attr("src", (d) => "img/gender/" + d.gender + ".png");
+            }
+        } else if (architectRemoved && firstBtnPressedAfterArchitectRemoved){
+            $("#ageImg")
+            .remove();
+            appendBackArchitects();
+            Pictures
+            .attr("class", "otherProperty gender")
+            .attr("src", (d) => "img/gender/" + d.gender + ".png");
+            PicturesContainer = Pictures;
+            firstBtnPressedAfterArchitectRemoved = false;
+            architectRemoved = false;  
+        }
+        
+    }
+
+// toggle nationBtn
+    $("#nationBtn")
+    .on("click", onClickNationBtn);
+
+    function onClickNationBtn(){
+        if(!architectRemoved){
+            if(!firstBtnPressedAfterArchitectRemoved){
+                PicturesContainer
+                .attr("class", "otherProperty nation")
+                .attr("src", (d) => "img/nation/" + d.nation + ".png");
+            } else {
+                pictures
+                .attr("class", "otherProperty nation")
+                .attr("src", (d) => "img/nation/" + d.nation + ".png");
+            }
+        } else if (architectRemoved && firstBtnPressedAfterArchitectRemoved){
+            $("#ageImg")
+            .remove();
+            appendBackArchitects();
+            Pictures
+            .attr("class", "otherProperty nation")
+            .attr("src", (d) => "img/nation/" + d.nation + ".png");
+            PicturesContainer = Pictures;
+            firstBtnPressedAfterArchitectRemoved = false;
+            architectRemoved = false;
+        } 
+    }
+// toggle glassBtn
+    $("#glassBtn")
+    .on("click", onClickGlassBtn);
+
+    function onClickGlassBtn(){
+        if(!architectRemoved){
+            if(!firstBtnPressedAfterArchitectRemoved){
+                PicturesContainer
+                .attr("class", "otherProperty glass")
+                .attr("src", (d) => "img/glass/" + d.glass + ".png");
+            } else {
+                pictures
+                .attr("class", "otherProperty glass")
+                .attr("src", (d) => "img/glass/" + d.glass + ".png");
+            }
+        } else if (architectRemoved && firstBtnPressedAfterArchitectRemoved){
+            $("#ageImg")
+            .remove();
+            appendBackArchitects();
+            Pictures
+            .attr("class", "otherProperty glass")
+            .attr("src", (d) => "img/glass/" + d.glass + ".png");
+            PicturesContainer = Pictures;
+            firstBtnPressedAfterArchitectRemoved = false;
+            architectRemoved = false;
+        } 
+    }
+// toggle ageBtn
+    $("#ageBtn")
+    .on("click", onClickAgeBtn);
+
+ function onClickAgeBtn(){
+    if(!architectRemoved){
+        architect
+        .remove();
+
+        section
+        .append("div")
+        .attr("id", "ageImg")
+        .attr("class", "divOfAgeImg")
+        .append("img")
+        .attr("class", "age")
+        .attr("src", "img/age.svg");
+    architectRemoved = true;
+    architectHadRemovedOnce = true;
+    }
+
+    if(architectHadRemovedOnce){
+        Architect
+        .remove();
+    }
+    firstBtnPressedAfterArchitectRemoved = true;
+}
+
+// toggle resetBtn
+    $("#resetBtn")
+    .on("click", onClickRestBtn);
+
+    function onClickRestBtn(){
+        if(!architectRemoved){
+            if(!firstBtnPressedAfterArchitectRemoved){
+                PicturesContainer
+                .attr("class", "photo")
+                .attr("src", (d) => "img/photo/" + d.img + ".png");
+            } else {
+                pictures
+                .attr("class", "photo")
+                .attr("src", (d) => "img/photo/" + d.img + ".png");
+            }
+        } else if (architectRemoved && firstBtnPressedAfterArchitectRemoved){
+            $("#ageImg")
+            .remove();
+            appendBackArchitects();
+            Pictures
+            .attr("class", "photo")
+            .attr("src", (d) => "img/photo/" + d.img + ".png");
+            PicturesContainer = Pictures;
+            firstBtnPressedAfterArchitectRemoved = false;
+            architectRemoved = false;
+        } 
+    }
+function appendBackArchitects(){
+
+let architect = section.selectAll('g')
+    .data(nest)
+    .enter()
+    .append('g')
+    .attr("id", "architect");
+
+Architect = architect;
+
+let pictures = architect.selectAll('img')
+    .data(function(d){
+    return d.values;
+})
+    .enter()
+    .append('img')
+    .attr("class", "photo")
+    .attr("src", (d) => "img/photo/" + d.img + ".png")
+    .on("mouseenter", ShowTooltip)
+    .on("mouseout", DismissTooltip);
 
     const index = d => d.index;
     function ShowTooltip(d){
@@ -39,46 +209,54 @@ let pictures = architect.selectAll('img')
     function DismissTooltip(d){
         d3.select("#tooltip" + index(d)).style("opacity", 0);
     }
+    Pictures = pictures;
+    ///////Building tooltip//////
+// add a tooltip to each architect
+let tooltip = architect
+.append('g')
+.attr("id", "tooltip")
+.attr("class", "tooltip")
+.style("opacity", 0);
 
-// toggle genderBtn
-    $("#genderBtn")
-    .on("click", onClickGenderBtn);
-    
-    function onClickGenderBtn(){
-        pictures
-        .attr("class", "otherProperty gender")
-        .attr("src", (d) => "img/gender/" + d.gender + ".png");
-    }
-// toggle nationBtn
+$("#section img")
+.each(function(i){
+$("#tooltip")
+.attr("id", "tooltip" + i)
+.appendTo($(this).parent());
+});
 
-    $("#nationBtn")
-    .on("click", onClickNationBtn);
+// add name to tooltip
+// construct the container
+let tooltipName = tooltip
+.append('div')
+.attr("class", "tooltipName");
+// add the value
+let name = tooltipName.selectAll('span')
+.data(function(d){
+    return d.values;
+})
+.enter()
+.append('span')
+.attr("id", "name")
+.text((d) => d.name)
 
-    function onClickNationBtn(){
-        pictures
-        .attr("class", "otherProperty nation")
-        .attr("src", (d) => "img/nation/" + d.nation + ".png");
-    }
-    $("#glassBtn")
-    .on("click", onClickGlassBtn);
-
-    function onClickGlassBtn(){
-        pictures
-        .attr("class", "otherProperty glass")
-        .attr("src", (d) => "img/glass/" + d.glass + ".png");
-    }
-// toggle resetBtn
-    $("#resetBtn")
-    .on("click", onClickRestBtn);
-
-    function onClickRestBtn(){
-        pictures
-        .attr("class", "photo")
-        .attr("src", (d) => "img/photo/" + d.img + ".png");
-    }
-
-console.log(nest)
-// add a group to each architect
+// add year to tooltip
+// construct the container
+let tooltipYear = tooltip
+.append('div')
+.attr("class", "tooltipYear")
+// add the value
+let year = tooltipYear.selectAll('span')
+.data(function(d){
+    return d.values;
+})
+.enter()
+.append('span')
+.attr("id", "year")
+.text((d) => d.year + "  Laureate");
+}
+///////Building tooltip//////
+// add a tooltip to each architect
 let tooltip = architect
     .append('g')
     .attr("id", "tooltip")
@@ -92,13 +270,12 @@ let tooltip = architect
     .appendTo($(this).parent());
     });
 
-    
-
 // add name to tooltip
+// construct the container
 let tooltipName = tooltip
     .append('div')
     .attr("class", "tooltipName");
-
+// add the value
 let name = tooltipName.selectAll('span')
     .data(function(d){
         return d.values;
@@ -107,11 +284,13 @@ let name = tooltipName.selectAll('span')
     .append('span')
     .attr("id", "name")
     .text((d) => d.name)
+
 // add year to tooltip
+// construct the container
 let tooltipYear = tooltip
     .append('div')
     .attr("class", "tooltipYear")
-
+// add the value
 let year = tooltipYear.selectAll('span')
     .data(function(d){
         return d.values;
@@ -121,8 +300,6 @@ let year = tooltipYear.selectAll('span')
     .attr("id", "year")
     .text((d) => d.year + "  Laureate");
     
-console.log(pictures)
-
 });
 
 
